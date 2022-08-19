4935c2a5764e50e622f6a609d90ed57f56744abd
sau114 <sau114@yandex.ru>
2022-08-19 14:46:32 +0300

    Add SpePcaWatchman

--- module_final/utils/watchmen.py
+++ module_final/utils/watchmen.py
@@ -1,9 +1,9 @@
-from typing import Optional
+from typing import Optional, Union
 
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import IncrementalPCA
+from sklearn.decomposition import IncrementalPCA, PCA
 
 
 class WatchmanError(ValueError):
@@ -16,8 +16,11 @@ class LimitWatchman:
 
     def __init__(self, ewma: Optional[str] = None):
         self.limits = None
-        self.halflife = ewma
+        self.ewma = ewma
         return
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(ewma={self.ewma})'
 
     def learn(self,
               data: pd.DataFrame,
@@ -30,8 +33,8 @@ class LimitWatchman:
             self.limits['hi'] = data.max()
         elif not self.limits.index.equals(data.columns):
             raise WatchmanError('Fields of limits is not equals to data columns')
-        if self.halflife is not None:
-            data = data.ewm(halflife=self.halflife, times=data.index.values).mean()
+        if self.ewma is not None:
+            data = data.ewm(halflife=self.ewma, times=data.index.values).mean()
         mean = (data.max() + data.min()) / 2
         half_range = (data.max() - data.min()) / 2
         self.limits['lo'] = self.limits['lo'].combine(mean - half_range * (1 + tolerance), min)
@@ -40,8 +43,8 @@ class LimitWatchman:
 
     def examine(self, data: pd.DataFrame) -> pd.DataFrame:
         # examine this data for anomalies
-        if self.halflife is not None:
-            data = data.ewm(halflife=self.halflife, times=data.index.values).mean()
+        if self.ewma is not None:
+            data = data.ewm(halflife=self.ewma, times=data.index.values).mean()
         result = (data < self.limits['lo']) | (data > self.limits['hi'])
         return result
 
@@ -49,49 +52,119 @@ class LimitWatchman:
 class LimitPcaWatchman:
     # Same as LimitWatchman, but in space of principal components.
 
-    def __init__(self, n_components: int):
-        self.limits = {}  # dict of watched values
+    def __init__(self, n_components: Union[str, int] = 'auto'):
+        self.limits = None
         self.scaler = StandardScaler()
-        self.pca = IncrementalPCA(n_components=n_components)
-        self._pc_names = tuple('pc'+str(i) for i in range(n_components))
+        self.n_components = n_components
+        if self.n_components == 'auto':
+            self.pca = IncrementalPCA(n_components=1)
+        elif isinstance(self.n_components, int) and self.n_components > 0:
+            self.pca = IncrementalPCA(n_components=self.n_components)
+        else:
+            raise WatchmanError('Wrong value of n_components')
         return
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(n_components={self.n_components})'
 
     def partial_fit_scaler(self, data: pd.DataFrame) -> None:
         self.scaler.partial_fit(data)
+        if self.n_components == 'auto':
+            pca = PCA(n_components='mle')
+            pca.fit(data)
+            self.pca.n_components = max(self.pca.n_components, pca.n_components_)
         return
 
     def partial_fit_pca(self, data: pd.DataFrame) -> None:
-        scaled_data = self.scaler.transform(data)
-        self.pca.partial_fit(scaled_data)
+        self.pca.partial_fit(self.scaler.transform(data))
         return
 
     def learn(self, data: pd.DataFrame, tolerance: float = 0.01) -> None:
         # learn and store limits of this data
         # watchman don't forget previous limits
+        pc_names = tuple('pc' + str(i) for i in range(self.pca.n_components))
         pc_data = pd.DataFrame(index=data.index,
-                               columns=self._pc_names,
+                               columns=pc_names,
                                data=self.pca.transform(self.scaler.transform(data)),
                                )
-        for c in pc_data.columns:
-            if c not in self.limits:
-                self.limits[c] = {
-                    'lo': pc_data[c].min(),
-                    'hi': pc_data[c].max(),
-                }
-            mean = (pc_data[c].max() + pc_data[c].min()) / 2
-            half_range = (pc_data[c].max() - pc_data[c].min()) / 2
-            self.limits[c]['lo'] = min(self.limits[c]['lo'], mean - half_range * (1 + tolerance))
-            self.limits[c]['hi'] = max(self.limits[c]['hi'], mean + half_range * (1 + tolerance))
+        if self.limits is None:
+            self.limits = pd.DataFrame(index=pc_data.columns, columns=['lo', 'hi'])
+            self.limits['lo'] = pc_data.min()
+            self.limits['hi'] = pc_data.max()
+        elif not self.limits.index.equals(pc_data.columns):
+            raise WatchmanError('Fields of limits is not equals to data columns')
+        mean = (pc_data.max() + pc_data.min()) / 2
+        half_range = (pc_data.max() - pc_data.min()) / 2
+        self.limits['lo'] = self.limits['lo'].combine(mean - half_range * (1 + tolerance), min)
+        self.limits['hi'] = self.limits['hi'].combine(mean + half_range * (1 + tolerance), max)
         return
 
     def examine(self, data: pd.DataFrame) -> pd.DataFrame:
         # examine this data for anomalies
+        pc_names = tuple('pc' + str(i) for i in range(self.pca.n_components))
         pc_data = pd.DataFrame(index=data.index,
-                               columns=self._pc_names,
+                               columns=pc_names,
                                data=self.pca.transform(self.scaler.transform(data)),
                                )
-        result = pd.DataFrame(index=pc_data.index, columns=pc_data.columns, data=0, dtype='uint8')
-        for c in pc_data.columns:
-            if c in self.limits:
-                result[c] = ~pc_data[c].between(self.limits[c]['lo'], self.limits[c]['hi'])
+        result = (pc_data < self.limits['lo']) | (pc_data > self.limits['hi'])
+        return result
+
+
+class SpePcaWatchman:
+    # Same as LimitPcaWatchman, but watch only for square prediction error (SPE) aka Q statistic.
+
+    def __init__(self, n_components: Union[str, int] = 'auto'):
+        self.limits = None
+        self.scaler = StandardScaler()
+        self.n_components = n_components
+        if self.n_components == 'auto':
+            self.pca = IncrementalPCA(n_components=1)
+        elif isinstance(self.n_components, int) and self.n_components > 0:
+            self.pca = IncrementalPCA(n_components=self.n_components)
+        else:
+            raise WatchmanError('Wrong value of n_components')
+        return
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(n_components={self.n_components})'
+
+    def partial_fit_scaler(self, data: pd.DataFrame) -> None:
+        self.scaler.partial_fit(data)
+        if self.n_components == 'auto':
+            pca = PCA(n_components='mle')
+            pca.fit(data)
+            self.pca.n_components = max(self.pca.n_components, pca.n_components_)
+        return
+
+    def partial_fit_pca(self, data: pd.DataFrame) -> None:
+        self.pca.partial_fit(self.scaler.transform(data))
+        return
+
+    def learn(self, data: pd.DataFrame, tolerance: float = 0.05) -> None:
+        # learn and store limits of this data
+        # watchman don't forget previous limits
+        rest_data = pd.DataFrame(index=data.index,
+                                 columns=data.columns,
+                                 data=self.pca.inverse_transform(self.pca.transform(self.scaler.transform(data))),
+                                 )
+        spe = ((data - rest_data) ** 2).mean(axis=1)
+        if self.limits is None:
+            self.limits = {
+                'lo': spe.min(),
+                'hi': spe.max(),
+            }
+        mean = (spe.max() + spe.min()) / 2
+        half_range = (spe.max() - spe.min()) / 2
+        self.limits['lo'] = min(self.limits['lo'], mean - half_range * (1 + tolerance))
+        self.limits['hi'] = max(self.limits['hi'], mean + half_range * (1 + tolerance))
+        return
+
+    def examine(self, data: pd.DataFrame) -> pd.DataFrame:
+        # examine this data for anomalies
+        rest_data = pd.DataFrame(index=data.index,
+                                 columns=data.columns,
+                                 data=self.pca.inverse_transform(self.pca.transform(self.scaler.transform(data))),
+                                 )
+        spe = ((data - rest_data) ** 2).mean(axis=1)
+        result = (spe < self.limits['lo']) | (spe > self.limits['hi'])
         return result
